fe30a3356ec0c036f99c3beb9d05e0a2bd35be7e
Alex Elgin <sascha.elgin@gmail.com>
2020-04-13 15:08:33 +0600

    fix ES5-compatibility again

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const remoteScriptLoader = require('remote-script-loader');
 const load = function (options) {
     return new Promise(function (resolve) {
         return remoteScriptLoader.loadScriptOnce(
-            `https://api-maps.yandex.ru/2.1/?lang=${options.lang || 'ru'}_${options.region || 'RU'}&apikey=${options.apiKey}`,
+            'https://api-maps.yandex.ru/2.1/?lang=' + (options.lang || 'ru') + '_' + (options.region || 'RU') +
+            '&apikey=' + options.apiKey,
             'ymaps',
         ).then(function () { ymaps.ready(resolve); });
     });
